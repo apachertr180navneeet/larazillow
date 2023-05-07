e3233f4b3c154a4a1d9b44c190fd60d70ff7c706
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2023-05-07 21:34:25 +0530

    edit form using vue and inertia

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
-  <Link href="/">Main Page</Link>&nbsp;
-  <Link href="/hello">Show Page</Link>
+  <Link href="/listing">Listing</Link>&nbsp;
+  <Link href="/listing/create">New Listing</Link>
   <!-- <div>The page with time {{ timer }}</div> -->
 
   <div v-if="flashSuccess" class="success">
@@ -17,7 +17,7 @@ import { Link, usePage } from '@inertiajs/inertia-vue3'
 // page.props.value.flash.success
 const page = usePage();
 const flashSuccess = computed(
-    () => page.props.value.flash.success
+  () => page.props.value.flash.success
 )
 // const timer = ref(0)
 // setInterval(() => timer.value++, 1000)
