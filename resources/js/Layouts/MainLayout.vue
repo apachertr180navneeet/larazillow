e2a91b00401bd80335aa05fac23c700334cd6ff0
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2023-05-08 22:37:08 +0530

    chages

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -19,7 +19,6 @@
   <main class="container mx-auto p-4">
     <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
       {{ flashSuccess }}
-      I am navneet
     </div>
     <slot>Default</slot>
   </main>
